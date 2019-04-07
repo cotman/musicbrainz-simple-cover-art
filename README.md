78c3834da1ecc5d00d4e1c1ef503afac39703313
cotman <cotman.mail@gmail.com>
2019-04-07 12:50:38 +0100

    Update to include (and mandate) a thumbnail, as well as the full image.

--- README.md
+++ README.md
@@ -2,7 +2,8 @@
 
 ## Background
 
-A very basic C++ example of using the [MusicBrainz](https://musicbrainz.org/) C++ libraries to retrieve cover art for album/artist combinations:
+A very basic example of using the [MusicBrainz](https://musicbrainz.org/) C++ libraries to retrieve thumbnail and full
+URLs to cover art for album and artist search combinations:
 
 * [`libmusicbrainz`](https://github.com/metabrainz/libmusicbrainz)
 * [`libcoverart`](https://github.com/metabrainz/libcoverart)
@@ -28,22 +29,19 @@ On Ubuntu:
     Artist: The Primitives
     Album: Lovely
     Search String: '"Lovely" artist:"The Primitives"'
-    ReleaseID: a5ba15e3-cf7f-4069-9e97-336a243ace3c
-    ReleaseID: 4c703590-7386-49ee-854a-a4e54ed00814
-    ReleaseID: b18ab80e-6cf1-4d8a-9b1d-cf8aa9595b4f
-    ReleaseID: 6ac0e685-66a0-3177-af1d-eb8f85b93ef1
-    ReleaseID: 7fa519bb-57e2-4c89-b5aa-c469fb9a6a89
-    ReleaseID: eb164a12-edd0-4d30-9783-d74b51d42ee8
-    ReleaseID: 2b8da3df-5924-46e5-83bd-542166d2c037
-    ReleaseID: 381b4fd0-0456-47c4-8c79-abe3a71402dd
-    ReleaseID: 52d60d25-87b6-40cd-890b-21ef8bbabfb4
-    ReleaseID: 50ee9e80-3850-4e79-8389-807a12467d93
-    Cover Art: http://coverartarchive.org/release/4c703590-7386-49ee-854a-a4e54ed00814/4457399400.jpg
-    Cover Art: http://coverartarchive.org/release/7fa519bb-57e2-4c89-b5aa-c469fb9a6a89/4457418228.jpg
-    Cover Art: http://coverartarchive.org/release/eb164a12-edd0-4d30-9783-d74b51d42ee8/8039434694.jpg
-    Cover Art: http://coverartarchive.org/release/2b8da3df-5924-46e5-83bd-542166d2c037/16298750844.jpg
-    Cover Art: http://coverartarchive.org/release/381b4fd0-0456-47c4-8c79-abe3a71402dd/11523050459.jpg
-    Cover Art: http://coverartarchive.org/release/50ee9e80-3850-4e79-8389-807a12467d93/2278048950.jpg
-
-    
-
+    Parsing MusicBrainz Release ID: a5ba15e3-cf7f-4069-9e97-336a243ace3c
+    Parsing MusicBrainz Release ID: 4c703590-7386-49ee-854a-a4e54ed00814
+    Parsing MusicBrainz Release ID: b18ab80e-6cf1-4d8a-9b1d-cf8aa9595b4f
+    Parsing MusicBrainz Release ID: 6ac0e685-66a0-3177-af1d-eb8f85b93ef1
+    Parsing MusicBrainz Release ID: 7fa519bb-57e2-4c89-b5aa-c469fb9a6a89
+    Parsing MusicBrainz Release ID: eb164a12-edd0-4d30-9783-d74b51d42ee8
+    Parsing MusicBrainz Release ID: 2b8da3df-5924-46e5-83bd-542166d2c037
+    Parsing MusicBrainz Release ID: 381b4fd0-0456-47c4-8c79-abe3a71402dd
+    Parsing MusicBrainz Release ID: 52d60d25-87b6-40cd-890b-21ef8bbabfb4
+    Parsing MusicBrainz Release ID: 50ee9e80-3850-4e79-8389-807a12467d93
+    Thumbnail: http://coverartarchive.org/release/2b8da3df-5924-46e5-83bd-542166d2c037/16298750844-250.jpg, Full: http://coverartarchive.org/release/2b8da3df-5924-46e5-83bd-542166d2c037/16298750844.jpg
+    Thumbnail: http://coverartarchive.org/release/381b4fd0-0456-47c4-8c79-abe3a71402dd/11523050459-250.jpg, Full: http://coverartarchive.org/release/381b4fd0-0456-47c4-8c79-abe3a71402dd/11523050459.jpg
+    Thumbnail: http://coverartarchive.org/release/4c703590-7386-49ee-854a-a4e54ed00814/4457399400-250.jpg, Full: http://coverartarchive.org/release/4c703590-7386-49ee-854a-a4e54ed00814/4457399400.jpg
+    Thumbnail: http://coverartarchive.org/release/50ee9e80-3850-4e79-8389-807a12467d93/2278048950-250.jpg, Full: http://coverartarchive.org/release/50ee9e80-3850-4e79-8389-807a12467d93/2278048950.jpg
+    Thumbnail: http://coverartarchive.org/release/7fa519bb-57e2-4c89-b5aa-c469fb9a6a89/4457418228-250.jpg, Full: http://coverartarchive.org/release/7fa519bb-57e2-4c89-b5aa-c469fb9a6a89/4457418228.jpg
+    Thumbnail: http://coverartarchive.org/release/eb164a12-edd0-4d30-9783-d74b51d42ee8/8039434694-250.jpg, Full: http://coverartarchive.org/release/eb164a12-edd0-4d30-9783-d74b51d42ee8/8039434694.jpg
--- get_cover_art.cpp
+++ get_cover_art.cpp
@@ -1,16 +1,27 @@
+/*
+   Simple example of using the MusicBrainz C++ libraries and APIs to return thumbnail and full image URLs
+   to cover art for album and artist search combinations.
+*/
+
+
+#include <map>
 #include <iostream>
+#include <iterator>
 
 #include "musicbrainz5/Query.h"
 #include "musicbrainz5/Release.h"
 #include "coverart/CoverArt.h"
 #include "coverart/Image.h"
 #include "coverart/ImageList.h"
+#include "coverart/Thumbnails.h"
 
-std::vector<std::string> get_cover_art(const std::string artist, const std::string album, const std::string query_limit)
+
+std::map<std::string, std::string> get_cover_art_urls(const std::string artist, const std::string album, const std::string query_limit)
 {
     std::cout << "Artist: " << artist << std::endl;
     std::cout << "Album: " << album << std::endl;
 
+    // Format: '"<ALBUM_NAME>" artist:"<ARTIST_NAME>"'
     std::string search_string = "\"" + album + "\"" + " artist:\"" + artist + "\"";
     std::cout << "Search String: \'" << search_string << "\'" << std::endl;
 
@@ -21,47 +32,61 @@ std::vector<std::string> get_cover_art(const std::string artist, const std::stri
 
     CoverArtArchive::CCoverArt CoverArt("cover-art-query");
 
-    std::vector <std::string> cover_art;
+    std::map<std::string, std::string> image_urls;
 
-    try 
+    try
     {
+        // First get a list of Releases that match the search criteria
         MusicBrainz5::CMetadata Metadata = Query.Query("release", "", "", Params);
         MusicBrainz5::CReleaseList *ReleaseList = Metadata.ReleaseList();
 
+        std::string image_thumbnail_url;
+        std::string image_full_url;
+
+        // Iterate the list of Releases and, for each, pluck out the unique Release ID
         for (int count = 0; count < ReleaseList->NumItems(); count++)
         {
             MusicBrainz5::CRelease *Release = ReleaseList->Item(count);
 
             std::string ReleaseID = Release->ID();
-            std::cout << "ReleaseID: " << ReleaseID << std::endl;
+            std::cout << "Parsing MusicBrainz Release ID: " << ReleaseID << std::endl;
 
+            // We have a Release ID, so query for the appropriate images
             try
             {
                 CoverArtArchive::CReleaseInfo ReleaseInfo = CoverArt.ReleaseInfo(ReleaseID);
-                CoverArtArchive::CImageList *ImageList = ReleaseInfo.ImageList();
+                CoverArtArchive::CImageList* ImageList = ReleaseInfo.ImageList();
 
                 for (int count = 0; count < ImageList->NumItems(); count++)
                 {
-                    CoverArtArchive::CImage *Image = ImageList->Item(count);
-                    if (Image && Image->Front())
+                    CoverArtArchive::CImage* Image = ImageList->Item(count);
+                    CoverArtArchive::CThumbnails* Thumbnails = Image->Thumbnails();
+
+                    if (!Image->Front() or Thumbnails->Small().empty())
                     {
-                        cover_art.push_back(Image->Image());
+                        continue;
                     }
+
+                    image_thumbnail_url = Thumbnails->Small();
+                    image_full_url = Image->Image();
+
+                    image_urls.insert(std::pair<std::string, std::string>(image_thumbnail_url, image_full_url));
                 }
             }
             catch (...)
             {
-                // Might fail, but we don't care
+                // No appropriate images were found for the Release ID, but we continue to check any others
             }
         }
     }
     catch (...)
     {
-        // Might fail, but we don't care
+        // There was an issue with MusicBrainz
     }
 
-    return cover_art;
+    return image_urls;
 }
+
 
 int main(int argc, char **argv)
 {
@@ -71,10 +96,13 @@ int main(int argc, char **argv)
         return 1;
     }
 
-    std::vector <std::string> cover_art = get_cover_art(argv[1], argv[2], argv[3]);
+    std::map<std::string, std::string> cover_art_urls = get_cover_art_urls(argv[1], argv[2], argv[3]);
+    std::map<std::string, std::string>::iterator iterator;
 
-    for (int i = 0; i < cover_art.size(); i++)     
-        std::cout << "Cover Art: " << cover_art[i] << std::endl;
+    for (iterator = cover_art_urls.begin(); iterator != cover_art_urls.end(); iterator++)
+    {
+        std::cout << "Thumbnail: " << iterator->first << ", Full: " << iterator->second << std::endl;
+    }
 
     return 0;
 }
